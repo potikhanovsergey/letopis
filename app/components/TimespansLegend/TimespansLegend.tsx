4d54cbb7439cecee84e504bd299ab3992e62f48b
sp <potikhanovsergey@gmail.com>
2024-02-04 14:30:44 +0300

    Add TimespansLegend

--- app/components/TimespansLegend/TimespansLegend.tsx
+++ app/components/TimespansLegend/TimespansLegend.tsx
@@ -1,14 +1,26 @@
 import { FC } from "react";
-import { Group } from "@mantine/core";
+import { ColorSwatch, Group, Text, Title, UnstyledButton } from "@mantine/core";
 
 import { TimespansLegendProps } from "./TimespansLegend.typings";
 
 export const TimespansLegend: FC<TimespansLegendProps> = ({ timespans }) => {
+  if (!timespans.length) return null;
+
   return (
-    <Group>
-      {timespans.map((timespan) => (
-        <div key={timespan.id}>{timespan.title}</div>
-      ))}
-    </Group>
+    <div>
+      <Title order={2} size="h3">
+        Легенда
+      </Title>
+      <Group>
+        {timespans.map((timespan) => (
+          <UnstyledButton key={timespan.id}>
+            <Group gap="xs">
+              <ColorSwatch radius="xs" size={16} color={timespan.color} />
+              <Text size="sm">{timespan.title}</Text>
+            </Group>
+          </UnstyledButton>
+        ))}
+      </Group>
+    </div>
   );
 };
